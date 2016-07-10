3da7547dd7cff45d6dc4a855807170e75c12d0dd
Eric W. Greene <eric@ericwgreene.com>
2016-07-10 09:04:12 -0700

    moved query to its own module

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,47 +1,5 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } from 'graphql';
-import { getWidget, getWidgets, getUser, getUsers } from '../database';
-
-import { userType} from './types/user-type';
-import { widgetType } from './types/widget-type';
+import { GraphQLSchema } from 'graphql';
+import { queryType } from './types/query-type';
 
 
-const query = new GraphQLObjectType({
-	name: 'Query',
-	fields: {
-		widget: {
-			type: widgetType,
-			description: 'Get a widget by id',
-			args: {
-				id: {
-					type: GraphQLString,
-					description: 'Id of the widget to get'
-				}
-			},
-			resolve: (ignore, args) => getWidget(args.id)
-		},
-		widgets: {
-			description: 'Get all widgets',
-			type: new GraphQLList(widgetType),
-			resolve: () => getWidgets()
-		},
-		user: {
-			type: userType,
-			description: 'Get a user by id',
-			args: {
-				id: {
-					type: GraphQLInt,
-					description: 'Id of the user to get'
-				}
-			},
-			resolve: (ignore, args) => getUser(args.id)
-		},
-		users: {
-			type: new GraphQLList(userType),
-			description: 'Get all users',
-			resolve: () => getUsers()
-		}
-	}
-
-});
-
-export const schema = new GraphQLSchema({ query });
+export const schema = new GraphQLSchema({ query: queryType });
---
+++ src/graphql/types/query-type.js
@@ -0,0 +1,44 @@
+import { GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList } from 'graphql';
+import { getWidget, getWidgets, getUser, getUsers } from '../../database';
+
+import { userType} from './user-type';
+import { widgetType } from './widget-type';
+
+export const queryType = new GraphQLObjectType({
+	name: 'Query',
+	fields: {
+		widget: {
+			type: widgetType,
+			description: 'Get a widget by id',
+			args: {
+				id: {
+					type: GraphQLString,
+					description: 'Id of the widget to get'
+				}
+			},
+			resolve: (ignore, args) => getWidget(args.id)
+		},
+		widgets: {
+			description: 'Get all widgets',
+			type: new GraphQLList(widgetType),
+			resolve: () => getWidgets()
+		},
+		user: {
+			type: userType,
+			description: 'Get a user by id',
+			args: {
+				id: {
+					type: GraphQLInt,
+					description: 'Id of the user to get'
+				}
+			},
+			resolve: (ignore, args) => getUser(args.id)
+		},
+		users: {
+			type: new GraphQLList(userType),
+			description: 'Get all users',
+			resolve: () => getUsers()
+		}
+	}
+
+});
